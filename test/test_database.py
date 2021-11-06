a31d7924de2b05c78b3c5a3b08dd526c3a5ff36b
Zeronix-Git <zerolyferr@gmail.com>
2021-11-07 00:03:21 +0800

    Parametrize pytest with IDs

--- test/test_database.py
+++ test/test_database.py
@@ -2,14 +2,14 @@ import pytest
 from war_room.core.types import User
 from war_room.core.database import SQLiteUserDatabase, TemporaryUserDatabase
 
-user_database_factories = (
-    lambda: TemporaryUserDatabase(),
-    lambda: SQLiteUserDatabase(':memory:')
-)
+user_database_factories = {
+    'temp': lambda: TemporaryUserDatabase(),
+    'sqlite': lambda: SQLiteUserDatabase(':memory:')
+}
 
 err_msg = "Unexpected error"
 
-@pytest.mark.parametrize('user_db_factory', user_database_factories)
+@pytest.mark.parametrize('user_db_factory', user_database_factories.values(), ids=user_database_factories.keys())
 def test_contains_user(user_db_factory):
     user_db = user_db_factory()
     assert not user_db.contains_user(0).expect(err_msg)
@@ -17,15 +17,15 @@ def test_contains_user(user_db_factory):
     assert user_db.update_user(user).is_ok
     assert user_db.contains_user(0).expect(err_msg)
 
-@pytest.mark.parametrize('user_db_factory', user_database_factories)
+@pytest.mark.parametrize('user_db_factory', user_database_factories.values(), ids=user_database_factories.keys())
 def test_get_user(user_db_factory):
     user_db = user_db_factory()
     user = User(id = 0)
     assert user_db.update_user(user).is_ok
     user = user_db.get_user(0).expect(err_msg).expect(err_msg)
     assert user.id == 0
-
-@pytest.mark.parametrize('user_db_factory', user_database_factories)
+    
+@pytest.mark.parametrize('user_db_factory', user_database_factories.values(), ids=user_database_factories.keys())
 def test_update_user_information(user_db_factory):
     user_db = user_db_factory()
     user = User(id = 0) 
